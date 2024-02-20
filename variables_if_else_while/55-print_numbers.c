e1d13d09e7f9e829f47bcf05ac72e81178ccf6a0
Carole <c.zenruffinen@hotmail.com>
2024-02-20 06:17:46 +0000

    Numbers

---
+++ variables_if_else_while/55-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of code
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+int number = 0;
+
+
+for (number = 0; number < 10; number++)
+{
+	printf("%d\n", number);
+
+}
+return (0);
+}
