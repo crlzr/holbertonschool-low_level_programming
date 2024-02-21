4f8074fd00d83395b8dd6c45319b30e7583023bb
Carole <c.zenruffinen@hotmail.com>
2024-02-21 04:28:24 +0000

    10x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * function print_alphabet_x10 - print alphabet 10 times
+ * Return: void
+ */
+
+
+void print_alphabet_x10(void)
+{
+int i;
+char a;
+
+for (i = 0; i < 10; ++i)
+{
+for (a = 'a'; a <= 'z'; ++a)
+{
+	_putchar('a');
+}
+	_putchar('\n');
+}
+}
