c2e7174f06a5a77f7145b815e733402e1a3f3502
Carole <c.zenruffinen@hotmail.com>
2024-02-21 04:52:53 +0000

    alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * function print_alphabet_x10 - print alphabet 10 times
- * Return: void
- */
-
+* print_alphabet_x10 - program that shows the alphabet 10 times
+* Description: Uses headers to link to another function and a nested loop
+* Return: int 0 when done and no errors
+*/
 
 void print_alphabet_x10(void)
 {
-int i;
-char a;
+	int i, j;
 
-for (i = 0; i < 10; ++i)
-{
-for (a = 'a'; a <= 'z'; ++a)
-{
-	_putchar('a');
-}
-	_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 25; j++)
+		{
+			_putchar(j + 'a');
+		}
+		_putchar(10);
+	}
 }
