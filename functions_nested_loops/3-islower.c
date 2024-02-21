5f4503e220a43a9d8a68ad76a4e3cccbd1110821
Carole <c.zenruffinen@hotmail.com>
2024-02-21 05:13:11 +0000

    lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c an int passed from the main file
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+
+int _islower(int c)
+{
+if (c >= 79 && c <= 122)
+
+	return (1);
+
+else
+	return (0);
+
+}
