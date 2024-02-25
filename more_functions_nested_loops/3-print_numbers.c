529c6fdc8b34969d0eed424c759c41892209375c
Carole <c.zenruffinen@hotmail.com>
2024-02-25 23:08:46 +0000

    print numbers 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 - 9
+ * _putchar: prints a single character
+ */
+
+void print_numbers(void)
+{
+char i = 48;
+
+for (i = 48; i <= 57; i++)
+{
+	_putchar (i);
+}
+	_putchar ('\n');
+}
