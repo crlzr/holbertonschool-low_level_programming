8ec3173bbe4678cd3602142853e832face2be266
Carole <c.zenruffinen@hotmail.com>
2024-02-26 06:02:58 +0000

    prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: int
+ */
+
+void print_square(int size)
+{
+	char row = 0;
+	char col = 0;
+
+	if (size > 0)
+	{
+		while (row < size)
+		{
+			col = 0;
+			while (col < size)
+			{
+				_putchar('#');
+				col++;
+			}
+
+		_putchar('\n');
+		row++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}	
