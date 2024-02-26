07fc45cd50c3a0000f3cedf35a55e207d2114642
Carole <c.zenruffinen@hotmail.com>
2024-02-26 08:32:53 +0000

    FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,29 @@ int main(void)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 
 
 			else if (number % 3 == 0)
 			{
-				printf("Fizz ");
+				printf("Fizz");
 			}
 
 				else if (number % 5 == 0)
 				{
-					printf("Buzz ");
+					printf("Buzz");
 				}
 					else
 					{
 						printf("%d ", number);
 
 					}
+		if (number != 100)
+		{
+			printf(" ");
+		}
 	}
-	printf("\n");
-	return (0);
+					printf("\n");
+					return (0);
 }
