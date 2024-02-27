108a77a56088894ee1f5c3ac6eec68715f69f731
Carole <c.zenruffinen@hotmail.com>
2024-02-27 03:20:26 +0000

    returns length of a string

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen - returns length of string
- * @s: string
+ * @s: int
  * Return: length
  */
 
 int _strlen(char *s)
 {
-	int length;
+	int length = 0;
 
 	while (*s != '\0')
 	{
