09640018bc4bc08d30d333b1b2806fc394425250
Carole <c.zenruffinen@hotmail.comgit config --global user.email c.zenruffinen@hotmail.com>
2024-02-28 23:10:18 +0000

    copy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies the string pointed to by src to the buffer pointed to by dest
+ * *_strcpy - copies the string pointed to 
+ * by src to the buffer pointed to by dest
  *
  *
  */
