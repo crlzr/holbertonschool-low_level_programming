9923d03b766441a72b6fff2d0ead929592e9c25a
Carole <c.zenruffinen@hotmail.comgit config --global user.email c.zenruffinen@hotmail.com>
2024-02-28 22:07:46 +0000

    print half a string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,40 @@
 #include "main.h"
 
 /**
- * puts_half - A function that prints every other character of a string.
+ * puts_half - Prints the second half of a string followed by a new line.
+ * @str: Pointer to the string.
  *
- * @str: input string
- *
- * Return: prints the second half of the string.
+ * This function calculates the length of the string @str and then prints
+ * the second half of the string (including the middle character if the
+ * length is odd), followed by a new line character '\n'.
  */
+
 void puts_half(char *str)
 {
+	int l;
 	int i;
-	int len;
+	int half;
 
-	len = 0;
-	while (str[len])
+	l = 0;
+	while (str[l] != '\0')
 	{
-		len = len + 1;
+		l++;
 	}
-
-	i = (len + 1) / 2;
-	while (i < len)
+	if (l % 2 == 0)
 	{
-		_putchar(str[1]);
-		i = i + 1;
+		half = l / 2;
+		for (i = half; i < l; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		half = (l - 1) / 2;
+		for (i = half + 1; i < l; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
