2acdf1a4fb6dcb2f7e130dbf7fa3da5d276cb271
Carole <c.zenruffinen@hotmail.comgit config --global user.email c.zenruffinen@hotmail.com>
2024-02-28 22:27:14 +0000

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
