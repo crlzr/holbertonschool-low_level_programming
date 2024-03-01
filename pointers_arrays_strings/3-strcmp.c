335b16ea9ec66f5b640abddebfe18b6ddc09b201
Carole <c.zenruffinen@hotmail.comm>
2024-03-01 00:06:16 +0000

    comparison

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one to compare
+ * @s2: string two to compare
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s1[i] != '\0' && j == 0)
+	{
+		j = s1[i] - s2[i];
+		i++;
+	}
+	return (j);
+}
