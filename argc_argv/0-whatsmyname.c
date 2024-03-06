47fc6ea25f72df2087e7beaa4e6fa5c5e2bda154
Carole <c.zenruffinen@hotmail.com>
2024-03-06 03:25:24 +0000

    program's name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- * write a program that prints its own name
+ * main - write a program that prints its own name
  * @argc: argument counter in command line
  * @argv: argument vector (strings)
  * Return: the name of the program
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-	{	
-		printf("%s\n",argv[0]);
+	{
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
