c49ba8dcd3079a353fab6c18ededa4553ed5b31d
Carole <c.zenruffinen@hotmail.com>
2024-03-11 03:13:00 +0000

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,51 +1,56 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * **alloc_grid - returns a pointer to a 2-dimensional array
+ * alloc_grid - A function that returns a pointer to
+ * a 2 dimensional array of integers.
+ *
  * @width: width of array
  * @height: height of array
  *
- * Return: NULL on failure, 0 or negative NULL
+ * Return: NULL if width or height is 0, else returns a 2D array of ints
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **grid = 0;
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
+	int **array; /* deref, to retrieve value from mem pointed to by ptr*/
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-
-	grid = malloc(height * sizeof(int *));
-
-	if (grid == NULL)
+	array = malloc(sizeof(int *) * height);
+	if (array == NULL)
 	{
 		return (NULL);
 	}
+	i = 0;
 	while (i < height)
 	{
-		grid[i] = malloc(sizeof(int) * width);
-		if (grid[i] == NULL)
+		array[i] = malloc(sizeof(int) * width);
+		if (array[i] == NULL)
 		{
 			j = 0;
 			while (j < i)
 			{
-		/* free memory of previously allocated row to avoid memory leaks */
-				free(grid[j]);
-				j++;
+		/* free memory of previously allocated row to avoid memory leaks*/
+				free(array[j]);
+				j = j + 1;
 			}
+			free(array);
+			return (NULL);
 		}
+		/* if malloc succeed, func initialize all to 0 and iterate each row and col*/
 		j = 0;
 		while (j < width)
 		{
-			grid[i][j] = 0;
-			j++;
+		/* pointer to access element at i-th row and j-th col*/
+			array[i][j] = 0;
+			j = j + 1;
 		}
-		i++;
+		i = i + 1;
 	}
-	return (grid);
+	return (array);/* returns a pointer to the newly created 2D array*/
 }
