6f43751962943bb64a8fd78c9e0e1a181f85ef73
Carole <c.zenruffinen@hotmail.comm>
2024-03-12 03:36:47 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	p = malloc((len1 + len2 + 1) * sizeof(char));
+	p = malloc((len1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (NULL);
