a996f93b34d3154358c5b6ea8cc1bb76677a9340
Carole <c.zenruffinen@hotmail.comm>
2024-03-12 03:04:55 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
-
+	
+	i = 0;
 	for (j = 0; j < n && s2[j] != '\0'; j++)
 		p[i + j] = s2[j];
 
