9d8d44070bc6c4a58891dd969a28b8598c738cac
Carole <c.zenruffinen@hotmail.comm>
2024-03-12 04:17:55 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,7 +11,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,5 +22,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
+	while (i < (nmemb * size))
+	{
+		p[i] = 0;
+		i++;
+	}
 	return (p);
 }
