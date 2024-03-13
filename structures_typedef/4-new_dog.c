7558105171fd189839737674cd78a782c9dec98f
Carole <c.zenruffinen@hotmail.com>
2024-03-13 23:05:50 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,46 +1,83 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * *new_dog - function that creates a new dog
- * Return: NULL if function fails
+ * _strdup - A function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter.
+ *
+ * @str: str to be duplicated
+ *
+ * Return: NULL if str = NULL, string duplicated if successful
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i = i + 1;
+	}
+
+	dup = malloc(i + 1);
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (dup == NULL)
+		{
+			return (NULL);
+		}
+		dup[i] = str[i];
+		i = i + 1;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+/**
+ * new_dog - Function that creates a new dog
+ *
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
+ * Return: NULL if function fails, else prints name, age and owner
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	dog_t *ptr;
+	char *namecpy;
+	char *ownercpy;
 
-	if (new_dog == NULL)
+	ptr = malloc(sizeof(*ptr));
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
-	/* Allocate memory for the name and owner of strings */
-
-	new_dog->name = malloc(strlen(name) +1);
-	new_dog->owner = malloc(strlen(owner) + 1);
-
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	namecpy = _strdup(name);
+	if (namecpy == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
-		free(new_dog);
+		free(ptr);
 		return (NULL);
 	}
-
-	/* Copy the name and owener strings */
-
-	strcpy(new_dog->name, name);
-	strcpy(new_dog->owner, owner);
-
-	/* Set the age */
-
-	new_dog->age = age;
-
-	return (new_dog);
+	ptr->name = namecpy;
+	ptr->age = age;
+	ownercpy = _strdup(owner);
+	if (ownercpy == NULL)
+	{
+		free(namecpy);
+		free(ptr);
+		return (NULL);
+	}
+	ptr->owner = ownercpy;
+	return (ptr);
 }
