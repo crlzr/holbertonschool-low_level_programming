1cb00228f3ede457656036f4d8fc5232f5900a81
Carole <c.zenruffinen@hotmail.comm>
2024-03-13 04:03:06 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - this is a dog structure file
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
