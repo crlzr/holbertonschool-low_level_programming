463257d54c938975ea2e796f66ead50f65480e3d
Carole <c.zenruffinen@hotmail.com>
2024-03-13 22:17:31 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,18 +2,20 @@
 #define MAIN_H
 
 /**
- * struct dog_t - this is a dog structure file
+ * struct dog - this is a dog structure file
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
 
-struct dog_t
+struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
