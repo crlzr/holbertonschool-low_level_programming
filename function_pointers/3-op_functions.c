8d250c5f33bc1f700a82d8fbdf36ca691a917d8b
Carole <c.zenruffinen@hotmail.com>
2024-03-18 03:02:16 +0000

    3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/** 
+ * op_add - returns the sum of a and b
+ * op_sub - returns the difference of a and b
+ * op_mul - returns the product of a and b
+ * op_div - returns the result of divison of a and b
+ * op_mod - returns the remainder of the division
+ * of and ab
+ */
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif
