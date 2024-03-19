774084289f695b8d2f4bb83e88d711f7412a456d
Carole <c.zenruffinen@hotmail.comm>
2024-03-19 00:45:16 +0000

    3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,28 +1,36 @@
-#include "3-op_functions.c"
-
 
 /**
- * get_op_func - pointer to function to select correct function
- * @s: the operator passed as argument to the program
- * Return: a pointer to the function that corresponds to
- * the operator given as a parameter
- * not allowed to use switch, for, do..while, goto, else
- * only one if statement, one while loop
- * Return: if s does not match NULL
+ * get_op_func - function pointer that selects the correct function
+ * to perform operation
+ * @s: pointer to char
+ * Return: result
+ *
  */
 
 int (*get_op_func(char *s))(int, int)
 {
-	
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	i = 0;
 
+	while (*ops[i].op != *s)
+	{
+		i++;
+
+		if ((i > 4 || s[1] != '\0'))
+		{
+			printf("Error\n");
+			exit(99);
+		}
+	}
+
+	return (ops[i].f);
 }
