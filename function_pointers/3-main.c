3ed9fcbc53404ff49bc30bf3b7c0cc63fdd89c1b
Carole <c.zenruffinen@hotmail.comm>
2024-03-19 00:44:54 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - main function 
  * Return: 0 on success
- * no loop allowed
- * 3 if statements
  *
  */
 
 
-int main (void)
+int main (int argc, char *argv[])
 {
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+		return (1);
+	}
 
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit (100);
+		return (1);
+	}
 }
