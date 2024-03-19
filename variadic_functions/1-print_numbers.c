c7b2903ee7a83d951b54519c486aee3cd964f084
Carole <c.zenruffinen@hotmail.comm>
2024-03-19 06:28:44 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -7,26 +7,29 @@
 /**
  * print_numbers - prints numbers, followed by nl
  * @separator: pointer to string to be printed bw numbers
- * @n: number to be passed in 
+ * @n: number to be passed in
  * Return: separator == 0 return 0, otherwise the number
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 
-       	va_list num;
+	va_list num;
 	unsigned int i;
 
-        va_start(num, n);
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d\n", va_arg(num, int)); 
-	}
+		printf("%d", va_arg(num, int));
 
-	if (i < n - 1 && separator != NULL) 
-	{
-		printf("%s", separator);
+
+		if (i < (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+
 	}
+	printf("\n");
 	va_end(num);
 }
