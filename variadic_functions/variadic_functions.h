13d98e81f5a7a1600209c9f45d7409a42f96675d
Carole <c.zenruffinen@hotmail.com>
2024-03-28 22:45:45 +0000

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,22 +4,24 @@
 #include <stdarg.h>
 
 /**
- * struct operations - ties a data type to a corresponding function
- * @tpye: the data type
- * @func: the function
+ * struct format - ties a data type to a corresponding function
+ * @type: the data type
+ * @f: the function
  *
  * Description: this struct stores a char with the corresponding function
  *              that prints the data type specified with the char
  */
-typedef struct operations
+
+void print_all(const char * const format, ...);
+
+struct format
 {
 	char type;
-	void (*func)(va_list *args);
-} operation;
+	void (*f)(va_list list);
+};
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif
