4a2d6c5ea6f1be649e772cdfcdf9324ca20235d1
Carole <c.zenruffinen@hotmail.com>
2024-03-31 03:36:50 +0000

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,21 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stddef.h>
+
+/**
+ * struct list_node - struct re singly linked list
+ * @str: strings to be printed
+ * @next: points to the next node
+ */
+typedef struct list_node
+{
+	char *str;
+	int len;
+	struct list_node *next;
+
+} list_t;
+
 size_t print_list(const list_t *h);
 
 
