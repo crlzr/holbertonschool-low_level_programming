cce35a7a3d08e2ff25dcf0f197b93448e960905f
Carole <c.zenruffinen@hotmail.com>
2024-04-02 05:12:34 +0000

    1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -4,22 +4,22 @@
 #include <stddef.h>
 
 /**
- * print_dlistint_len - function to print all elements of list
+ * dlistint_len - function to print all elements of list
  * @h: head pointer
  * Return: no. of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t i = 0;
-	int len;
+	size_t i = 0;
+	int len = 0;
 
-        while (h != NULL)
-        {
-                i++;
+	while (h != NULL)
+	{
+		i++;
 		len = len + 1;
-                h = h->next;
-        }
+		h = h->next;
+	}
 
-        return (len);
+	return (len);
 }
