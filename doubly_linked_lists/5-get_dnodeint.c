82f14180cbb22e06aa731e1910fbe37a099b0e53
Carole <c.zenruffinen@hotmail.comm>
2024-04-03 02:18:18 +0000

    5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 	dlistint_t *current = head;
 
-	if (head == NULL)
+	if (head == NULL || current == NULL)
 	{
 		return (NULL);
 	}
