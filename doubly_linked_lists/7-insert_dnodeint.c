4f2433720a9adf4fb5ee7b7ec9e43e316d65d3eb
Carole <c.zenruffinen@hotmail.comm>
2024-04-03 05:50:34 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->prev = NULL;
 	}
 
-	if (idx = 0)
+	if (idx == 0)
 	{
 		new_node->next = current;
 		(*h)->prev = new_node;
