ebf2a73ca3e2ba4cdfc10234cfb53e6447a524e4
Carole <c.zenruffinen@hotmail.comm>
2024-04-03 06:14:07 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,40 +10,40 @@
  * @n: the data
  * Return: address of new node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *temp;
-    unsigned int i;
+	dlistint_t *new_node, *temp;
+	unsigned int i;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-    if (idx == 0 || *h == NULL) {
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        new_node->next = *h;
-        *h = new_node;
-        return new_node;
-    }
+	if (idx == 0 || *h == NULL)
+	{
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
 
-    temp = *h;
-    for (i = 0; i < idx - 1 && temp != NULL; i++)
-        temp = temp->next;
-    if (temp == NULL)
-        return NULL;
+	temp = *h;
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
 
-    new_node->prev = temp;
-    new_node->next = temp->next;
-    if (temp->next != NULL)
-        temp->next->prev = new_node;
-    temp->next = new_node;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
 
-    return new_node;
+	return (new_node);
 }
 
