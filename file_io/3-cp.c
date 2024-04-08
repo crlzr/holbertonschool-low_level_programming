29072dd4b32756eee084e94a5fddcc607d2e611b
Carole <c.zenruffinen@hotmail.com>
2024-04-08 05:38:05 +0000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,11 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stddef.h>
+
 /**
  * main - check the code
  * @ac: argc
