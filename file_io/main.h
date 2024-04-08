f928afa027fc64b5b7e47e4b0c35fc99152df5a8
Carole <c.zenruffinen@hotmail.com>
2024-04-08 05:35:18 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <stddef.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
