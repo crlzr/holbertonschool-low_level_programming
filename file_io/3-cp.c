cc171a1bd53b9ac4d4ce3c75b2ee60055fa24437
Carole <c.zenruffinen@hotmail.com>
2024-04-08 04:25:54 +0000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,52 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <fcntl.h>
 
-int main(int argc, char *argv[]) 
+/**
+ * main - function that copies the contents of one file to another.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
 {
+	/* Declarations */
 	int file_from, file_to, bytes_read, bytes_written;
 	char buffer[1024];
 
-	if (argc != 3) {
+	/* Argument validation */
+	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	/* Opening file_from */
 	file_from = open(argv[1], O_RDONLY);
-
-	if (file_from == -1) {
+	if (file_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (file_to == -1) {
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+	/* Opening file_to */
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	while ((bytes_read = read(file_from, buffer, 1024)) > 0) {
-		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read) {
-			dprintf(STDERR_FILENO, "Error: Write error to file %s\n", argv[2]);
-			exit(99);
-		}
-	}
-
-	if (bytes_read == -1) {
-		dprintf(STDERR_FILENO, "Error: Read error from file %s\n", argv[1]);
+	/* Reading from file_from */
+	bytes_read = read(file_from, buffer, 1024);
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	if (close(file_from) == -1 || close(file_to) == -1) {
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
-		exit(100);
+	/* Writing to file_to */
+	bytes_written = write(file_to, buffer, bytes_read);
+	if (bytes_written == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 
-	return 0;
+	/* Closing file descriptors */
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
+	return (0);
 }
 
