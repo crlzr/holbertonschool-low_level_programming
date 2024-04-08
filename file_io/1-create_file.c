0532ad03b1668bb8f345dd8e4ee94fd84ed944f5
Carole <c.zenruffinen@hotmail.com>
2024-04-08 01:44:03 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
 	write(filedescriptor, text_content, i);
 	
-	if (filedescriptor == 0)
+	if (filedescriptor == -1)
 	{
 		return (-1);
 	}
